a58c4d6468511580f965d62c302622c9bf2cfb98
Pulkit-PB <pulkitb2004.pb@gmail.com>
2023-08-26 20:03:51 +0530

    Organizing into proper folders

---
+++ Introduction/helloworld.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    char nm[20];
+    printf("hello world\n");
+    printf("enter your name:");
+    scanf("%s",&nm);
+    printf("Welcome %s",nm);
+
+    return 0;
+}
